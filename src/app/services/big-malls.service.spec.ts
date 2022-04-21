db3530491f4c72eeabe40b5a6298e0b45e287c9c
HenryAr <henryarturorivera@gmail.com>
2022-04-21 10:50:48 +0200

    big-malls.service.spect

--- src/app/services/big-malls.service.spec.ts
+++ src/app/services/big-malls.service.spec.ts
@@ -1,13 +1,48 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { Component } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ZoneModel } from "../models/common/zone.model";
+import { BigMallsService } from "./big-malls.service";
 
-describe ('', () => {
-
-  let spy: any;
-
+describe ('BigMallsService', () => {
+  
+  let service: BigMallsService;
+  
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers:[
+        BigMallsService
+      ]
+    }).compileComponents();
+    
+    service = TestBed.inject(BigMallsService)
+  });
+
+  it('should be created',()=>{
+    expect(service).toBeDefined()
+    expect(service).toBeInstanceOf(BigMallsService)
+  });
+
+  it('#zoneSelected should return "[]" as default',()=>{
+    expect(service.zoneSelected).toBeTruthy();
+    expect(service.zoneSelected).toEqual([]);
+  });
+
+  it('#activitiesSelected should return "[]" as default',()=>{
+    expect(service.activitiesSelected).toBeTruthy();
+    expect(service.activitiesSelected).toEqual([]);
+  });
+
+  it('#setZonesSelected should inicialice zoneSelected as default',()=>{
+    let economicModel = {idActivity: 1, activityName: 'activity'}
+    service.setActivitiesSelected([economicModel])
+
+    expect(service.activitiesSelected).toEqual([economicModel]);
 
   });
 
-  it('',()=>{
-
-  });
+  
 });
