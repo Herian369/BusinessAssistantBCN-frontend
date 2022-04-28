a1d04c7e062cb26e7d430a0dea79f0cb01dcb6d9
HenryAr <henryarturorivera@gmail.com>
2022-04-28 13:32:25 +0200

    test big-malls.service

--- src/app/services/big-malls.service.spec.ts
+++ src/app/services/big-malls.service.spec.ts
@@ -1,48 +1,79 @@
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { Component } from "@angular/core";
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { TestBed } from "@angular/core/testing";
+import { EconomicActivityModel } from "../models/common/economic-activity.model";
 import { ZoneModel } from "../models/common/zone.model";
 import { BigMallsService } from "./big-malls.service";
 
 describe ('BigMallsService', () => {
-  
-  let service: BigMallsService;
-  
-  beforeEach(() => {
+  let httpSpy: {get:jasmine.Spy}
+  let service: BigMallsService
+
+  beforeEach(() => { 
+    httpSpy = jasmine.createSpyObj('HttpClient', ['get'])
+
     TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule
       ],
       providers:[
-        BigMallsService
+        BigMallsService,
+        {provide: HttpClient, useValue: httpSpy}
       ]
-    }).compileComponents();
-    
-    service = TestBed.inject(BigMallsService)
+    });    
   });
+  
+  beforeEach(()=>{
+    service = TestBed.inject(BigMallsService)    
+  })
+  
 
   it('should be created',()=>{
-    expect(service).toBeDefined()
-    expect(service).toBeInstanceOf(BigMallsService)
+    expect(service).toBeTruthy()
   });
 
-  it('#zoneSelected should return "[]" as default',()=>{
-    expect(service.zoneSelected).toBeTruthy();
-    expect(service.zoneSelected).toEqual([]);
+  it('_zonesSelected should be created', ()=>{
+    expect(service['_zonesSelected']).toBeTruthy()
+    expect(service['_zonesSelected']).toEqual([])
+  })
+
+  it('_activitiesSelected should be created', ()=>{
+    expect(service['_activitiesSelected']).toBeTruthy()
+    expect(service['_activitiesSelected']).toEqual([])
+  })
+
+  it('zoneSelected should return _zonesSelected',()=>{
+    const zone = service['_zonesSelected']
+    expect(service.zoneSelected).toEqual(zone);
   });
 
-  it('#activitiesSelected should return "[]" as default',()=>{
-    expect(service.activitiesSelected).toBeTruthy();
-    expect(service.activitiesSelected).toEqual([]);
+  it('activivitiesSelected should return _activitiesSelected',()=>{
+    const activities = service['_activitiesSelected']
+    expect(service.activitiesSelected).toEqual(activities);
   });
 
-  it('#setZonesSelected should inicialice zoneSelected as default',()=>{
-    let economicModel = {idActivity: 1, activityName: 'activity'}
-    service.setActivitiesSelected([economicModel])
-
-    expect(service.activitiesSelected).toEqual([economicModel]);
-
+  it('#setZonesSelected should change zoneSelected',()=>{
+    const zoneModel:ZoneModel[] = [{idZone: 1, zoneName: 'zone'}]
+    service.setZonesSelected(zoneModel)
+    expect(service.zoneSelected.length).toEqual(1)
   });
+  
+  it('#setActivitiesSelected should change activitiesSelected',()=>{
+    const economicModel:EconomicActivityModel[] = [{idActivity: 34768, activityName: 'activity'}]
+    service.setActivitiesSelected(economicModel)
+    expect(service.activitiesSelected.length).toBe(1)
+  });
+  
+  it('#getSelectedData should return the data of big-malls-fake-filtered', ()=>{
+    
+    service.getSelectedData().subscribe(data => {
+
+    })
+
+    
+    
+
+  })
 
   
 });
