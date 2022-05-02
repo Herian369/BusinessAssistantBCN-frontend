ffd47dd563fa93119ab0bf1cdc4c23145265144e
HenryAr <henryarturorivera@gmail.com>
2022-05-02 16:47:23 +0200

    big malls services done

--- src/app/services/big-malls.service.spec.ts
+++ src/app/services/big-malls.service.spec.ts
@@ -1,79 +1,110 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
+import { environment } from "src/environments/environment";
 import { EconomicActivityModel } from "../models/common/economic-activity.model";
 import { ZoneModel } from "../models/common/zone.model";
 import { BigMallsService } from "./big-malls.service";
 
 describe ('BigMallsService', () => {
-  let httpSpy: {get:jasmine.Spy}
+  let http: HttpTestingController
   let service: BigMallsService
 
   beforeEach(() => { 
-    httpSpy = jasmine.createSpyObj('HttpClient', ['get'])
 
     TestBed.configureTestingModule({
       imports: [
-        HttpClientTestingModule
+        HttpClientTestingModule,
       ],
       providers:[
         BigMallsService,
-        {provide: HttpClient, useValue: httpSpy}
+        {provide: HttpClient}
       ]
     });    
   });
   
   beforeEach(()=>{
-    service = TestBed.inject(BigMallsService)    
+    service = TestBed.inject(BigMallsService)
+    http = TestBed.inject(HttpTestingController) 
   })
   
-
-  it('should be created',()=>{
-    expect(service).toBeTruthy()
-  });
-
-  it('_zonesSelected should be created', ()=>{
-    expect(service['_zonesSelected']).toBeTruthy()
-    expect(service['_zonesSelected']).toEqual([])
-  })
-
-  it('_activitiesSelected should be created', ()=>{
-    expect(service['_activitiesSelected']).toBeTruthy()
-    expect(service['_activitiesSelected']).toEqual([])
-  })
-
-  it('zoneSelected should return _zonesSelected',()=>{
-    const zone = service['_zonesSelected']
-    expect(service.zoneSelected).toEqual(zone);
-  });
-
-  it('activivitiesSelected should return _activitiesSelected',()=>{
-    const activities = service['_activitiesSelected']
-    expect(service.activitiesSelected).toEqual(activities);
-  });
-
-  it('#setZonesSelected should change zoneSelected',()=>{
-    const zoneModel:ZoneModel[] = [{idZone: 1, zoneName: 'zone'}]
-    service.setZonesSelected(zoneModel)
-    expect(service.zoneSelected.length).toEqual(1)
-  });
-  
-  it('#setActivitiesSelected should change activitiesSelected',()=>{
-    const economicModel:EconomicActivityModel[] = [{idActivity: 34768, activityName: 'activity'}]
-    service.setActivitiesSelected(economicModel)
-    expect(service.activitiesSelected.length).toBe(1)
-  });
-  
-  it('#getSelectedData should return the data of big-malls-fake-filtered', ()=>{
+  describe('BigMallsService should be created', ()=>{
     
-    service.getSelectedData().subscribe(data => {
+    it('should be created',()=>{
+      expect(service).toBeTruthy()
+    });
+
+  })
+
+
+  describe('Variables should be inicialized', ()=>{
+    
+    it('_zonesSelected should be created', ()=>{
+      expect(service['_zonesSelected']).toBeDefined()
+      expect(service['_zonesSelected']).toBeTruthy()
+      expect(service['_zonesSelected']).toEqual([])
+    })
+
+    it('_activitiesSelected should be created', ()=>{
+      expect(service['_activitiesSelected']).toBeDefined()
+      expect(service['_activitiesSelected']).toBeTruthy()
+      expect(service['_activitiesSelected']).toEqual([])
+    })
+
+  })
+
+  describe('Methods getters and setters should be created', ()=>{
+
+    it('zoneSelected should return _zonesSelected',()=>{
+      const zone = service['_zonesSelected']
+      const serviceZone = service.zoneSelected
+      expect(serviceZone).toEqual(zone);
+    });
+  
+    it('activivitiesSelected should return _activitiesSelected',()=>{
+      const activities = service['_activitiesSelected']
+      expect(service.activitiesSelected).toEqual(activities);
+    });
+
+    it('#setZonesSelected should change zoneSelected',()=>{
+      const zoneModel:ZoneModel[] = [{idZone: 1, zoneName: 'zone'}]
+      service.setZonesSelected(zoneModel)
+      expect(service.zoneSelected.length).toEqual(1)
+    });
+    
+    it('#setActivitiesSelected should change activitiesSelected',()=>{
+      const economicModel:EconomicActivityModel[] = [{idActivity: 34768, activityName: 'activity'}]
+      service.setActivitiesSelected(economicModel)
+      expect(service.activitiesSelected.length).toBe(1)
+    });
+  })
+
+
+  describe('GET', ()=>{
+
+    it('#getSelectedData should match request by matcher function', ()=>{
+      
+      const result = 'testing'
+
+      service.getSelectedData().subscribe(data => {
+        expect(data).toEqual(result),
+        fail
+      })
+  
+      const req = http.expectOne( req => req.url === `${environment.BACKEND_BASE_URL}${environment.BACKEND_BIG_MALLS_FAKE_FILTERED_RESULTS}`)
+
+      expect(req.request.method).toBe('GET')
+      expect(req.request.params.get('zones')).toBe('[]')
+      expect(req.request.params.get('activities')).toBe('[]')
+
+      req.flush(result)
+      http.verify()
 
     })
 
-    
-    
-
   })
+
+  
 
   
 });
